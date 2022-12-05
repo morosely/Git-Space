6ec6555dd55117821a9c1e932aba237f835e4d23
yihaitao <yihaitao@e-future.com.cn>
2022-12-05 15:10:52 +0800

    文件监控

---
+++ Basic-Code-Example/wacth-files/src/main/java/com/shiji/file/WatchFiles.java
@@ -0,0 +1,51 @@
+package com.shiji.file;
+
+import cn.hutool.core.io.watch.WatchMonitor;
+import cn.hutool.core.io.watch.Watcher;
+import cn.hutool.core.io.watch.watchers.DelayWatcher;
+import cn.hutool.core.lang.Console;
+
+import java.nio.file.Path;
+import java.nio.file.WatchEvent;
+
+
+public class WatchFiles {
+
+    public static void main(String[] args) {
+        wacthPath("D:\\WorkSpaces\\My-Code");
+    }
+
+    public static void wacthPath(String path){
+        //File file = FileUtil.file("example.properties");
+        //这里只监听文件或目录的修改事件
+        WatchMonitor watchMonitor = WatchMonitor.createAll(path,new DelayWatcher(new Watcher(){
+            @Override
+            public void onCreate(WatchEvent<?> event, Path currentPath) {
+                Object obj = event.context();
+                Console.log("创建：{}-> {}", currentPath, obj);
+            }
+
+            @Override
+            public void onModify(WatchEvent<?> event, Path currentPath) {
+                Object obj = event.context();
+                Console.log("修改：{}-> {}", currentPath, obj);
+            }
+
+            @Override
+            public void onDelete(WatchEvent<?> event, Path currentPath) {
+                Object obj = event.context();
+                Console.log("删除：{}-> {}", currentPath, obj);
+            }
+
+            @Override
+            public void onOverflow(WatchEvent<?> event, Path currentPath) {
+                Object obj = event.context();
+                Console.log("Overflow：{}-> {}", currentPath, obj);
+            }
+        }, 5000));
+        //设置监听目录的最大深入，目录层级大于制定层级的变更将不被监听，默认只监听当前层级目录
+        watchMonitor.setMaxDepth(3);
+        //启动监听
+        watchMonitor.start();
+    }
+}
