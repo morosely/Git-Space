a4ac17800f532517bfb01f2859987cf60954c8f0
haitao.yi <haitao.yi@shijiretailgroup.com>
2023-03-13 19:48:29 +0800

    CAS

---
+++ Basic-Code-Example/tools/src/main/java/com/shiji/thread/atomic/DecimalAccount.java
@@ -0,0 +1,45 @@
+package com.shiji.thread.atomic;
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.function.IntUnaryOperator;
+
+public interface DecimalAccount {
+    // 获取余额
+    public BigDecimal getBalance();
+    // 取款
+    public void withdraw(BigDecimal amount);
+    /**
+     * 方法内会启动 1000 个线程，每个线程做 -10 元 的操作
+     * 如果初始余额为 10000 那么正确的结果应当是 0
+     */
+    public static void runningThread(DecimalAccount account) {
+        List<Thread> ts = new ArrayList<>();
+        for (int i = 0; i < 1000; i++) {
+            ts.add(new Thread(() -> {
+                account.withdraw(BigDecimal.TEN);
+            }));
+        }
+        ts.forEach(Thread::start);
+        ts.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println(account.getBalance());
+    }
+
+    public static void updateAndGet(AtomicInteger atomicInteger, IntUnaryOperator operator){
+        while (true) {
+            int prev = atomicInteger.get();
+            int next = operator.applyAsInt(prev);
+            if (atomicInteger.compareAndSet(prev, next)) {
+                break;
+            }
+        }
+    }
+}
---
+++ Basic-Code-Example/tools/src/main/java/com/shiji/thread/atomic/DecimalAccountSafeCas.java
@@ -0,0 +1,38 @@
+package com.shiji.thread.atomic;
+
+import java.math.BigDecimal;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class DecimalAccountSafeCas implements DecimalAccount {
+
+    private AtomicReference<BigDecimal> ref;
+
+    public DecimalAccountSafeCas(BigDecimal balance) {
+        ref = new AtomicReference<>(balance);
+    }
+
+    @Override
+    public BigDecimal getBalance() {
+        return ref.get();
+    }
+
+    @Override
+    public void withdraw(BigDecimal amount) {
+        while (true) {
+            BigDecimal prev = ref.get();
+            BigDecimal next = prev.subtract(amount);
+            if (ref.compareAndSet(prev, next)) {
+                break;
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        //DecimalAccount.runningThread(new DecimalAccountSafeCas(new BigDecimal("10000")));
+
+        AtomicInteger atomicInteger = new AtomicInteger(30);
+        DecimalAccount.updateAndGet(atomicInteger, p -> p / 10);
+        System.out.println("atomicInteger = " + atomicInteger.get());
+    }
+}
