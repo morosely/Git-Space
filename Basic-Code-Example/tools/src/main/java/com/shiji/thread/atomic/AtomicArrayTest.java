a3fb68fb49298d5ad37231c326be2cd3b719ed2e
yihaitao <yihaitao@e-future.com.cn>
2023-05-15 09:51:09 +0800

    原子数组

--- Basic-Code-Example/tools/src/main/java/com/shiji/thread/atomic/AtomicArrayTest.java
+++ Basic-Code-Example/tools/src/main/java/com/shiji/thread/atomic/AtomicArrayTest.java
@@ -42,7 +42,7 @@ public class AtomicArrayTest {
      参数4，打印数组的方法
      // supplier 提供者 无中生有 ()->结果
      // function 函数 一个参数一个结果 (参数)->结果 , BiFunction (参数1,参数2)->结果
-     // consumer 消费者 一个参数没结果 (参数)->void, BiConsumer (参数1,参数2)->
+     // consumer 消费者 一个参数没结果 (参数)->void, BiConsumer (参数1,参数2)->void
      */
     private static <T> void demo(Supplier<T> arraySupplier, Function<T, Integer> lengthFun,
                                  BiConsumer<T, Integer> putConsumer, Consumer<T> printConsumer ) {
