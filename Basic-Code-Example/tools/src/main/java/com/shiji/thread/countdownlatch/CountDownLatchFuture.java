b692d5e982d0eb487ec7142e473ee3f869bc977b
yihaitao <yihaitao@e-future.com.cn>
2023-08-25 15:24:26 +0800

    ReadWriteLock

--- Basic-Code-Example/tools/src/main/java/com/shiji/thread/countdownlatch/CountDownLatchFuture.java
+++ Basic-Code-Example/tools/src/main/java/com/shiji/thread/countdownlatch/CountDownLatchFuture.java
@@ -1,15 +1,22 @@
 package com.shiji.thread.countdownlatch;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 @Slf4j
-public class CountDownLatchController {
+public class CountDownLatchFuture {
 
     public static void main(String[] args) throws Exception {
         RestTemplate restTemplate = new RestTemplate();
@@ -39,5 +46,26 @@ public class CountDownLatchController {
         service.shutdown();
     }
 
+    @Bean
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        //先获取到converter列表
+        List<HttpMessageConverter<?>> converters = builder.build().getMessageConverters();
+        for(HttpMessageConverter<?> converter : converters){
+            //因为我们只想要jsonConverter支持对text/html的解析
+            if(converter instanceof MappingJackson2HttpMessageConverter){
+                try{
+                    //先将原先支持的MediaType列表拷出
+                    List<MediaType> mediaTypeList = new ArrayList<>(converter.getSupportedMediaTypes());
+                    //加入对text/html的支持
+                    mediaTypeList.add(MediaType.TEXT_HTML);
+                    //将已经加入了text/html的MediaType支持列表设置为其支持的媒体类型列表
+                    ((MappingJackson2HttpMessageConverter) converter).setSupportedMediaTypes(mediaTypeList);
+                }catch(Exception e){
+                    e.printStackTrace();
+                }
+            }
+        }
+        return builder.build();
+    }
 
 }
