58571c8edd8209107da452d1b34f41841387da34
yihaitao <yihaitao@126.com>
2024-02-08 15:21:11 +0800

    自动注入Bean

--- Template-Project/springboot-bean/my-springboot-mybatis/src/main/java/com/haitao/MySpringbootMybatisApplication.java
+++ Template-Project/springboot-bean/my-springboot-mybatis/src/main/java/com/haitao/MySpringbootMybatisApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.ComponentScan;
+
 @SpringBootApplication
 @ComponentScan(basePackages = "com.yihaitao")
 public class MySpringbootMybatisApplication {
