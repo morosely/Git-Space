b1f432c20202406e3623e947ec24151704852b65
yihaitao <yihaitao@126.com>
2024-04-08 16:12:08 +0800

    delete

--- Template-Project/code-interview/src/main/java/com/itheima/pojo/Son.java
+++
@@ -1,7 +0,0 @@
-package com.itheima.pojo;
-
-import org.springframework.stereotype.Component;
-
-@Component
-public class Son {
-}
--- Template-Project/code-interview/src/main/java/com/itheima/processor/Teacher.java
+++
@@ -1,4 +0,0 @@
-package com.itheima.processor;
-
-public class Teacher {
-}
