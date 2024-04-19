9356dd99114ff5ccac23a396aaf090834a8ffd60
yihaitao <yihaitao@126.com>
2024-04-19 16:05:49 +0800

    多数据源提交

--- Template-Project/springboot-multi-data-source/multi-datasource-mybatis/src/test/java/com/xkcoding/multi/datasource/mybatis/MultiDatasourceMybatisApplicationTests.java
+++ Template-Project/springboot-multi-data-source/multi-datasource-mybatis/src/test/java/com/xkcoding/multi/datasource/mybatis/MultiDatasourceMybatisApplicationTests.java
@@ -1,17 +1,45 @@
 package com.xkcoding.multi.datasource.mybatis;
 
+import cn.hutool.json.JSONUtil;
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.xkcoding.multi.datasource.mybatis.model.User;
+import com.xkcoding.multi.datasource.mybatis.service.UserService;
+import lombok.extern.slf4j.Slf4j;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.List;
+
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@Slf4j
 public class MultiDatasourceMybatisApplicationTests {
 
+    @Autowired
+    private UserService userService;
+
+    /**
+     * 主从库添加
+     */
     @Test
-    public void contextLoads() {
+    public void addUser() {
+        User userMaster = User.builder().name("主库添加").age(20).build();
+        userService.addUser(userMaster);
+
+        User userSlave = User.builder().name("从库添加").age(20).build();
+        userService.save(userSlave);
     }
 
+    /**
+     * 从库查询
+     */
+    @Test
+    public void testListUser() {
+        List<User> list = userService.list(new QueryWrapper<>());
+        log.info("【list】= {}", JSONUtil.toJsonStr(list));
+    }
 }
 
