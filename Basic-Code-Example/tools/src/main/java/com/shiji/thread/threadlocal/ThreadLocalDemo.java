58060be678c82ca926b6ee5bbf5d6e416b282fcc
yihaitao <yihaitao@126.com>
2024-09-25 11:01:29 +0800

    ThreadLocalDemo

---
+++ Basic-Code-Example/tools/src/main/java/com/shiji/thread/threadlocal/ThreadLocalDemo.java
@@ -0,0 +1,47 @@
+package com.shiji.thread.threadlocal;
+
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+class House {
+    int saleCount = 0;
+
+    public synchronized void saleHouse() {
+        saleCount++;
+    }
+
+    ThreadLocal<Integer> saleVolume = ThreadLocal.withInitial(() -> 0);
+
+    public void saleVolumeByThreadLocal() {
+        saleVolume.set(1 + saleVolume.get());
+    }
+
+
+}
+
+public class ThreadLocalDemo {
+    public static void main(String[] args) {
+        House house = new House();
+        for (int i = 1; i <= 5; i++) {
+            new Thread(() -> {
+                int size = new Random().nextInt(5) + 1;
+                try {
+                    for (int j = 1; j <= size; j++) {
+                        house.saleHouse();
+                        house.saleVolumeByThreadLocal();
+                    }
+                    System.out.println(Thread.currentThread().getName() + "\t" + "号销售卖出：" + house.saleVolume.get());
+                } finally {
+                    house.saleVolume.remove();
+                }
+            }, String.valueOf(i)).start();
+
+        }
+        try {
+            TimeUnit.MILLISECONDS.sleep(300);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        System.out.println(Thread.currentThread().getName() + "\t" + "共计卖出多少套： " + house.saleCount);
+    }
+}
